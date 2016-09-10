6fcb412f55b14065f4b9007808be787f554a2eb3
Scott Barr <scottjbarr@gmail.com>
2016-09-10 12:12:05 +1000

    Combine ip and port flags into listen flag.

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 )
 
 var (
-	ip   = flag.String("ip", "", "Address to bind to")
-	port = flag.Int("port", 8080, "Port to listen")
-	path = flag.String("path", "./", "Path served as document root.")
+	listen = flag.String("listen", ":8080", "Address to bind to")
+	path   = flag.String("path", "./", "Path served as document root.")
 )
 
 const Version = "0.1.1"
@@ -23,14 +22,12 @@ func main() {
 		fmt.Printf("Error: %v", err)
 	}
 
-	address := fmt.Sprintf("%v:%v", *ip, *port)
-
 	fmt.Printf("Static file server running at %v. CTRL + C to shutdown\n",
-		address)
+		*listen)
 
 	handler := http.FileServer(http.Dir(docroot))
 
-	if err = http.ListenAndServe(address, handler); err != nil {
+	if err = http.ListenAndServe(*listen, handler); err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 }
